72f395bf8230245ec6d4e392d6a55ea12b5ef64d
timothy1706036-ops <timothy.1706036@bpkpenabur.sch.id>
2026-09-22 20:45:15 +0700

    Implement initial structure for Rufier test application with user input handling

--- main.py
+++ main.py
@@ -11,3 +11,59 @@ from instructions import *
 from ruffier import *
 
 from seconds import Seconds
+
+age = 7
+name = ""
+p1, p2, p3 = 0, 0, 0
+
+def check_int(str_num):
+    try:
+        return int(str_num)
+    except ValueError:
+        return False
+
+class InstrScr(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        instr = Label(text=txt_instruction)
+
+        lbl1 = Label(text="Enter your name:", halign = 'right')
+        self.in_name = TextInput(multiline=False)
+        lbl2 = Label(text="Enter your age:", halign = 'right')
+        self.in_age = TextInput(text='', multiline=False)
+        self.btn = Button(text='start', size_hint=(0.3, 0.2), pos_hint={'center_x': 0.5})
+        self.btn.on_press = self.next
+
+        line1 = BoxLayout(size_hint=(0.8,None), height='30sp')
+        line2 = BoxLayout(size_hint=(0.8,None), height='30sp')
+        line1.add_widget(lbl1)
+        line1.add_widget(self.in_name)
+        line2.add_widget(lbl2)
+        line2.add_widget(self.in_age)
+
+        outer = BoxLayout(orientation='vertical', padding=8, spacing=8)
+        outer.add_widget(instr)
+        outer.add_widget(line1)
+        outer.add_widget(line2)
+        outer.add_widget(self.btn)
+
+        self.add_widget(outer)
+
+    def next(self):
+        name = self.in_name.text
+        age = check_int(self.in_age.text)
+        if age == False or age < 7:
+            age = 7
+            self.in_age.text = str(age)
+        else:
+            self.manager.current = 'pulse1'
+
+class HeartCheck(App):
+    def build(self):
+        sm = ScreenManager()
+        sm.add_widget(InstrScr(name='instr'))
+
+        return sm
+
+app = HeartCheck()
+app.run()
