7d92ebb67e02539ae52c049889fc4154de5a6ff3
timothy1706036-ops <timothy.1706036@bpkpenabur.sch.id>
2026-09-22 20:19:20 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from kivy.app import App
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.textinput import TextInput
+from kivy.core.window import Window
+from kivy.uix.scrollview import ScrollView
+
+from instructions import *
+from ruffier import *
+
+from seconds import Seconds
